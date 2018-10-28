13f249cdbc2d308e1556466d75243d3f7247ecb2
amitp <amitpatel_7@msn.com>
2018-10-28 13:00:20 -0700

    solved Basic Algorithm #5 - Return Largest Numbers in Arrays

--- algorithms.js
+++ algorithms.js
@@ -73,3 +73,17 @@
     }
     
     findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+/*
+    5.) Return Largest Numbers in Arrays
+
+    Return an array consisting of the largest number from each provided sub-array.
+    For simplicity, the provided array will contain exactly 4 sub-arrays.
+    Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+*/
+
+    /* Solution */
+
+    const largestOfFour = (arr) => arr.map(sub_arr => Math.max(...sub_arr));
+
+    largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
