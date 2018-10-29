57b176999fdd112ca852c237ef0110bd471fbb49
amitp <amitpatel_7@msn.com>
2018-10-29 10:47:40 -0700

    solved Basic Algorithms #9 - Finders Keepers

--- algorithms.js
+++ algorithms.js
@@ -152,3 +152,21 @@
       }
       
       truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+/*
+      9.) Finders Keepers
+
+      Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+      If no element passes the test, return undefined.
+*/
+
+      /* Solution */
+
+      const findElement = (arr, func) => {
+        let num = 0;
+        let filtered = arr.filter(func);
+      
+        return filtered[0];
+      }
+      
+      findElement([1, 2, 3, 4], num => num % 2 === 0);
